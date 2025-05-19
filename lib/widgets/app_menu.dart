b448a04745ca86d809cc4aac104605c6e5b49ded
AGM Khair Sabbir <sabbirhossain832@gmail.com>
2025-05-19 23:41:24 +0600

    menu design change

--- lib/widgets/app_menu.dart
+++ lib/widgets/app_menu.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/providers/navigation_provider.dart';
 import 'package:provider/provider.dart';
-
-import '../providers/navigation_provider.dart';
 
 class AppMenu extends StatelessWidget {
   const AppMenu({super.key});
@@ -22,21 +21,29 @@ class AppMenu extends StatelessWidget {
     return Row(
       children: List.generate(
         items.length,
-        (index) {
+            (index) {
+          final bool isSelected = provider.currentIndex == index;
+
           return TextButton(
             onPressed: () => context.read<NavigationProvider>().setIndex(index),
-            child: Text(
-              items[index],
-              style: TextStyle(
-                color: provider.currentIndex == index
-                    ? Colors.amber
-                    : Colors.white,
+            style: ButtonStyle(
+              // Change background/overlay color on hover
+              overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                    (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.hovered)) {
+                    return Colors.amber.withOpacity(0.2); // Your hover color here
+                  }
+                  return null; // default
+                },
+              ),
+              foregroundColor: MaterialStateProperty.all<Color>(
+                isSelected ? Colors.amber : Colors.white,
               ),
             ),
+            child: Text(items[index]),
           );
         },
       ),
     );
   }
 }
-
